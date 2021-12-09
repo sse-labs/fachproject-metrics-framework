ca53f9da24d95727794ae36cdb085f755e1e3f41
Mehdi <mehdihassanpour96@gmail.com>
2021-12-09 01:14:08 +0100

    - Arranged the code for implementation - Issue #20

--- src/main/scala/impl/demo/DefaultSingleFileAnalysisApplication.scala
+++ src/main/scala/impl/demo/DefaultSingleFileAnalysisApplication.scala
@@ -3,11 +3,13 @@ package impl.demo
 
 import analysis.SingleFileAnalysis
 import application.SingleFileAnalysisApplication
+import impl.group3.LCCAnalysis
 
 object DefaultSingleFileAnalysisApplication extends SingleFileAnalysisApplication {
 
   override protected val registeredAnalyses: Seq[SingleFileAnalysis] = Seq(
-    new NumberOfMethodsAnalysis(),
-    new MethodSizeAnalysis()
+//    new NumberOfMethodsAnalysis(),
+//    new MethodSizeAnalysis(),
+      new LCCAnalysis()
   )
 }
