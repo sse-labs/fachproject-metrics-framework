ab83e1a2b57d46b2e330446301d9ed4a28269ef7
Johannes Düsing <j.duesing13@googlemail.com>
2021-12-09 15:14:20 +0100

    Add TypeAnalysis interface

---
+++ src/main/scala/analysis/TypeAnalysis.scala
@@ -0,0 +1,31 @@
+package org.tud.sse.metrics
+package analysis
+
+import input.CliParser.OptionMap
+
+import org.opalj.br.ObjectType
+import org.opalj.br.analyses.Project
+
+import java.net.URL
+import scala.util.{Success, Try}
+
+trait TypeAnalysis extends SingleFileAnalysis {
+
+  override final def analyzeProject(project: Project[URL],
+                                    customOptions: OptionMap): Try[Iterable[MetricValue]] = {
+    val typeResults = project
+      .projectClassFiles
+      .map(_.thisType)
+      .map( typeObj => analyzeType(typeObj, project, customOptions))
+
+    if(typeResults.exists(_.isFailure)){
+      typeResults.find(_.isFailure).get
+    } else {
+      Success(typeResults.flatMap(_.get))
+    }
+
+  }
+
+  def analyzeType(typeObj: ObjectType, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]]
+
+}
