fecdb726c4ee43384a717163c4de83d0d71708c3
Sebastian <1sebastian1@online.de>
2021-12-11 18:33:51 +0100

    NumberOfLoopsAnalysis besitzt jetzt optionale Argumente #22

--- src/main/scala/impl/group5/NumberOfLoopsAnalysis.scala
+++ src/main/scala/impl/group5/NumberOfLoopsAnalysis.scala
@@ -15,10 +15,21 @@ import scala.util.Try
  * Die Klasse NumberOfLoopsAnalysis stellt die impliktion  der Metrik Number of Loops (Loop) da.
  * Diese Metrik zählt für jede Methode die anzahl der Schleifen und gibt sie aus.
  *
+ * Die Default einstellung ist das nur die Methoden ausgeben werden in denn Loops sich befinden. Die anderen Methoden wo Loops 0 ist werden weggelassen
+ *
+ * Optional CLI argument:
+ *  - --only-class Es wird nur die Anzahl an Loops in einer Klasse angezeigt und nicht für jede Methode
+ *  - --out-all-loops Es werden alle Methoden ausgeben, auch die mit Loop 0.
+ *  - --no-class Es wird nicht die gesamt Zahl der Loops einer Klasse ausgeben
+ *  - --only-class-with-loops Gibt nur Klassen mit Loops aus.
  */
 
 class NumberOfLoopsAnalysis extends ClassFileAnalysis {
 
+  private val onlyClassSymbol: Symbol = Symbol("only-class")
+  private val outAllSymbol: Symbol = Symbol("out-all-loops")
+  private val noClassSymbol: Symbol = Symbol("no-class")
+  private  val onlyClassWithLoopsSymbol: Symbol = Symbol("only-class-with-loops")
 
   /**
    * Die Methode führt die Metrik Loop aus.
@@ -29,9 +40,20 @@ class NumberOfLoopsAnalysis extends ClassFileAnalysis {
    */
   override def analyzeClassFile(classFile: ClassFile, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
 
+    val outAllMethode = customOptions.contains(outAllSymbol)
+    val onlyClass = customOptions.contains(onlyClassSymbol)
+    val noClass = customOptions.contains(noClassSymbol)
+    val onClassLoops = customOptions.contains(onlyClassWithLoopsSymbol)
+
+    if(noClass &&(onlyClass||onClassLoops))
+      {
+        println("Fehler Beide argumente dürfen nicht gleichzeitig gesetzt sein")
+        List.empty
+      }
+
     val methods= classFile.methodsWithBody
     var classloops = 0
-    var rlist = new ListBuffer[MetricValue]
+    val rlist = new ListBuffer[MetricValue]()
     while(methods.hasNext) {
       val method = methods.next()
       val body = method.body
@@ -56,11 +78,26 @@ class NumberOfLoopsAnalysis extends ClassFileAnalysis {
 
       }
       classloops += loops
-      rlist +=MetricValue(method.fullyQualifiedSignature, this.analysisName, loops)
-      println(MetricValue(method.fullyQualifiedSignature, this.analysisName, loops))
+
+      if(loops>0 && !onlyClass) {
+        rlist += MetricValue(method.fullyQualifiedSignature, this.analysisName, loops)
+      }
+      else if(outAllMethode)
+        {
+          rlist += MetricValue(method.fullyQualifiedSignature, this.analysisName, loops)
+        }
+
     }
-    println(MetricValue(classFile.fqn, this.analysisName, classloops))
-    List.empty
+
+    if(!noClass)
+    {
+      if(onClassLoops && classloops>0.0) {
+        rlist += MetricValue("Class:" + classFile.thisType.fqn, this.analysisName, classloops)
+      }
+      else if(!onClassLoops) rlist += MetricValue("Class:" + classFile.thisType.fqn, this.analysisName, classloops)
+    }
+
+    rlist.toList
 
 
 
