025e39205bb30cd1d3dd5cc5a8651b7b5aee160b
X1999-1 <59253340+X1999-1@users.noreply.github.com>
2021-12-14 13:53:47 +0100

    LCOM wird für alle Klassen berechnet
    es wird jetzt der LCOM-Wert für alle Klassen eines Projektes berechnet und in einer Liste zurückgegeben

--- src/main/scala/impl/group2/LackOfCohesionInMethodsAnalysis.scala
+++ src/main/scala/impl/group2/LackOfCohesionInMethodsAnalysis.scala
@@ -11,40 +11,45 @@ import scala.util.Try
 
 class LackOfCohesionInMethodsAnalysis extends SingleFileAnalysis{
 
-  override def analyzeProject(project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try{
+  override def analyzeProject(project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
+    val list: List[MetricValue] = List()
     val classes = project.allProjectClassFiles
-    val c = classes.head
-    val methods = c.methods
-    var metric = 0 // Wert der LCOM-Metrik
 
-    // es wird fuer alle Methodenpaare geprueft, ob die Attributmengen disjunkt sind
-    // wenn ja, wird metric um 1 erhöht, sonst um 1 verringert
-    for (i <- methods; j <- methods
-         if(!i.equals(j))){
-      if (disjunctAttributeSets(i, j)){
-        metric = metric + 1
-      } else {
-        metric = metric - 1
-      }
-    }
+    classes.foreach(singleClass => {
 
-    // in der Definition ist LCOM nie negativ, das Minimum ist 0
-    if (metric < 0){
-      metric = 0
-    }
+      val methods = singleClass.methods
+      var metric = 0 // Wert der LCOM-Metrik
 
-    // disjunctAttributeSets prueft, ob die Methoden m1 und m2 ausschliesslich auf unterschiedliche Attribute zugreifen
-    def disjunctAttributeSets(m1: Method, m2: Method): Boolean = {
-      var bool = true
-      for (i <- m1.attributes; j <- m2.attributes){
-        if (i.equals(j)) {
-          bool = false
+      // es wird fuer alle Methodenpaare geprueft, ob die Attributmengen disjunkt sind
+      // wenn ja, wird metric um 1 erhöht, sonst um 1 verringert
+      for (i <- methods; j <- methods
+           if (!i.equals(j))) {
+        if (disjunctAttributeSets(i, j)) {
+          metric = metric + 1
+        } else {
+          metric = metric - 1
         }
       }
-      bool
-    }
 
-    List(MetricValue("file", "LCOM", metric))
+      // in der Definition ist LCOM nie negativ, das Minimum ist 0
+      if (metric < 0) {
+        metric = 0
+      }
+
+      // disjunctAttributeSets prueft, ob die Methoden m1 und m2 ausschliesslich auf unterschiedliche Attribute zugreifen
+      def disjunctAttributeSets(m1: Method, m2: Method): Boolean = {
+        var bool = true
+        for (i <- m1.attributes; j <- m2.attributes) {
+          if (i.equals(j)) {
+            bool = false
+          }
+        }
+        bool
+      }
+
+      list.::(MetricValue("file", "LCOM", metric))
+    })
+    list
   }
 
   override def analysisName: String = "LCOM"
