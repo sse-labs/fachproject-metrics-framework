c5d1ff9ec5fbed0728384add92cf0fbfd90e242c
Helmi97 <chawachih@gmail.com>
2021-12-19 22:14:26 +0100

    correct how the metric is implemented

--- src/main/scala/impl/group4/LOCphyAnalysis.scala
+++ src/main/scala/impl/group4/LOCphyAnalysis.scala
@@ -11,31 +11,16 @@ import java.net.URL
 import scala.util.Try
 
 class LOCphyAnalysis extends MethodAnalysis{
-  override def analyzeMethod(m: Method, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try{
-    if(project.isProjectType(m.classFile.thisType)){
-      var result=0
-      if(m.body.isDefined) {
-        if(m.body.get.lineNumberTable.isDefined) {
-          result = m.body.get.lineNumberTable.get.lineNumbers.length
-          // Voids have 1 extra line probably because of the return line https://stackoverflow.com/questions/42673288/does-return-at-the-end-of-a-void-method-in-java-have-any-impact-on-performanc/42673380
-          /*if(m.returnType==VoidType){
-            result=result-1
-          }*/
-        }
-        List(MetricValue(m.fullyQualifiedSignature, "method.locphy", result))
-      }else{
-        log.warn(m.fullyQualifiedSignature+" could not be analyzed")
-        List.empty
-      }
-    } else {
-      List.empty
+  /**
+   * Calculates the value of the LOCphy Metric by subtracting the line numbers of the last and first elements of the line-number-table .
+   */
+  override def analyzeMethod(m: Method, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
+    if (project.isProjectType(m.classFile.thisType) && m.body.isDefined && m.body.get.lineNumberTable.isDefined) {
+      List(MetricValue(m.fullyQualifiedSignature, "method.locphy", m.body.get.lineNumberTable.get.lineNumbers.last.lineNumber - m.body.get.lineNumberTable.get.lineNumbers.head.lineNumber))
     }
-
-
-  }
-
+    else List.empty}
   /**
    * The name for this analysis implementation. Will be used to include and exclude analyses via CLI.
    */
-  override def analysisName: String = "method.locphy"
+  override def analysisName: String = "locphy"
 }
