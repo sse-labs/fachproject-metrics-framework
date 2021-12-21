37a88fb344a3197f1bcf61652a6c74987d76855d
Sebastian Kurth <1sebastian1@online.de>
2021-12-21 18:48:41 +0100

    Kommentare für cli arguments eingefügt und rückgabe Liste implemmentiert

--- src/main/scala/impl/group5/NumberOfVariablesDeclaredAnalysis.scala
+++ src/main/scala/impl/group5/NumberOfVariablesDeclaredAnalysis.scala
@@ -1,30 +1,43 @@
 package org.tud.sse.metrics
 package impl.group5
 
-import analysis.{SingleFileAnalysis, MetricValue}
+import analysis.{MetricValue, SingleFileAnalysis}
 import input.CliParser.OptionMap
 
 import org.opalj.br.analyses.Project
 
-
 import java.net.URL
+import scala.collection.mutable.ListBuffer
 import scala.util.Try
 
+
+
+/**
+ * Die Klasse NumberOfVariablesDeclaredAnalysis zählt die Anzahl der vorhanden Variablen in Klassen
+ * Dabei werden sowohl Klassenargumente als auch Locale Varaiblen gezählt und ausgeben
+ *
+ * Die Default Einstellung ist, dass alles ausgeben wird
+ *
+ * Optional CLI argument:
+ *  - --no-methode Es werden nur Klassen ausgeben
+ *  - --no-class Es werden nur Methoden ausgeben
+ *
+ */
 class NumberOfVariablesDeclaredAnalysis extends SingleFileAnalysis {
 
-  private val noMethodVariablesSymbol: Symbol = Symbol("no-method-variables")
-  private val onlyMethodVariablesSymbol: Symbol = Symbol("only-method-variables")
+  private val noMethodSymbol: Symbol = Symbol("no-method")
+  private val noClassSymbol: Symbol = Symbol("no-class")
 
-
+//TODO Varaiblen werden doppelt gezählt.
   override def analyzeProject(project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
 
-    var noMethodVariables = customOptions.contains(noMethodVariablesSymbol)
-    var onlyMethodVariables = customOptions.contains(onlyMethodVariablesSymbol)
-
-    if (noMethodVariables && onlyMethodVariables) {
+    var noMethod = customOptions.contains(noMethodSymbol)
+    var noClass = customOptions.contains(noClassSymbol)
+    val rlist = new ListBuffer[MetricValue]()
+    if (noMethod && noClass) {
       println("Nur Methodenvariablen und keine Methodenvaraiblen können nicht zusammen ausgewählt werden. Das Ergebnis wird mit Standart Optionen ausgegeben.")
-      noMethodVariables = false
-      onlyMethodVariables = false
+      noMethod = false
+      noClass = false
     }
 
     val classes = project.allProjectClassFiles
@@ -41,21 +54,23 @@ class NumberOfVariablesDeclaredAnalysis extends SingleFileAnalysis {
       var temporaryMethodVariablesSum = 0
       //Counts only Class variables
       var temporaryClassVariables = 0
-      if (!onlyMethodVariables) {
+      if (!noClass) {
         for (f <- c.fields) {
           println("Field is : " + f.fieldType)
           temporaryClassVariables = temporaryClassVariables + 1
           numberOfClassVariables = numberOfClassVariables + 1
         }
         println("Anzahl von Klassenvariablen in " +c.toString()+ " beträgt: "+temporaryClassVariables)
+        rlist += MetricValue("Anzahl von Klassenvariablen in " +c.fqn,this.analysisName, temporaryClassVariables)
       }
       //Getting all variables declared in Methods
-      if (!noMethodVariables) {
+      if (!noMethod) {
         if (c.methods != null) {
           for (m <- c.methodsWithBody) {
             //Counts local Variables for this Method only
             var temporaryMethodVariables = 0
             //Check if method body has local variables table
+            //TODO Klassenvairable werden mit gezählt
             val localVariableTable = m.body.get.localVariableTable
             if (localVariableTable.nonEmpty) {
               numberOfMethodVariables = numberOfMethodVariables + localVariableTable.get.size
@@ -64,9 +79,11 @@ class NumberOfVariablesDeclaredAnalysis extends SingleFileAnalysis {
 
             }
             println("Anzahl lokaler Variablen in "+m.toString()+ " beträgt: "+temporaryMethodVariables)
+            rlist += MetricValue("Anzahl lokaler Variablen in "+m.fullyQualifiedSignature, this.analysisName,temporaryMethodVariables)
           }
         }
         println("Anzahl von lokalen Variablen in allen Methoden von "+c.toString()+" beträgt: "+temporaryMethodVariablesSum)
+        rlist += MetricValue("Anzahl von lokalen Variablen in allen Methoden von "+c.fqn, this.analysisName, temporaryMethodVariablesSum)
       }
     }
     println("------------------------------------------------------------------------------------------------------------------")
@@ -75,10 +92,13 @@ class NumberOfVariablesDeclaredAnalysis extends SingleFileAnalysis {
     println("Anzahl deklarierter Variablen in allen Methoden: " + numberOfMethodVariables)
     println("------------------------------------------------------------------------------------------------------------------")
     metric = numberOfMethodVariables + numberOfClassVariables
+    rlist +=MetricValue("Anzahl deklarierter Variablen in allen Klassen: ", this.analysisName, numberOfClassVariables)
+    rlist +=MetricValue("Anzahl deklarierter Variablen in allen Methoden: ", this.analysisName, numberOfMethodVariables)
 
     //TODO In welchem Format Ergebnis zurückgeben? Für Klassen aufgeteilt oder ganzes Projekt
     MetricValue("Project:" + project.toString(), this.analysisName, metric)
-    List(MetricValue("file", "VDEC", metric))
+    rlist +=MetricValue("file", "VDEC", metric)
+    rlist.toList
   }
 
   override def analysisName: String = "VariablesDeclared.count"
