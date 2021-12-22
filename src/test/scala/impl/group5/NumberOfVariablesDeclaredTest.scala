dd5ca6de935caa1069d2cb94d4d74a753d87acb0
VidRuntimes <joe.langefeld@googlemail.com>
2021-12-22 21:50:01 +0100

    Tests for undefined variables

--- src/test/scala/impl/group5/NumberOfVariablesDeclaredTest.scala
+++ src/test/scala/impl/group5/NumberOfVariablesDeclaredTest.scala
@@ -2,8 +2,8 @@ package org.tud.sse.metrics
 package impl.group5
 
 import testutils.AnalysisTestUtils
-
 import org.scalatest.{FlatSpec, Matchers}
+
 
 import java.io.File
 
@@ -11,7 +11,7 @@ class NumberOfVariablesDeclaredTest extends FlatSpec with Matchers{
 
 
 
-  "The MethodSizeAnalysis" must "calculate valid results for single JARs" in {
+  "The Variables Declared Analysis" must "calculate valid results for single JARs" in {
 
     val analysisToTest = new NumberOfVariablesDeclaredAnalysis()
 
@@ -42,6 +42,21 @@ class NumberOfVariablesDeclaredTest extends FlatSpec with Matchers{
     val metricsResultDemoWrongOptions = resultDemoWrongOptions.head
     assert(!metricsResultDemoWrongOptions.metricValues.exists(value => value.entityIdent.equals("Class:testclass") && value.metricValue == 17.0))
 
+    //Undefined
+    val resultDemoUndefinedVariables = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestDemo, appConfig, Map(Symbol("undefined-variables") -> true))
+    val metricsResultDemoUndefinedVariables = resultDemoUndefinedVariables.head
+    assert(!metricsResultDemoUndefinedVariables.metricValues.exists(value => value.entityIdent.equals("Class:testclassUndev") && value.metricValue == 2.0))
+
+    val resultDemoUndefinedVariablesOnlyMethod = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestDemo, appConfig, Map(Symbol("undefined-variables") -> true,Symbol("no-class") -> true))
+    val metricsResultDemoUndefinedVariablesOnlyMethod = resultDemoUndefinedVariablesOnlyMethod.head
+    assert(!metricsResultDemoUndefinedVariablesOnlyMethod.metricValues.exists(value => value.entityIdent.equals("Class:testclassUndev") && value.metricValue == 1.0))
+
+    val resultDemoUndefinedVariablesOnlyClass = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestDemo, appConfig, Map(Symbol("undefined-variables") -> true,Symbol("no-method") -> true))
+    val metricsResultDemoUndefinedVariablesOnlyClass = resultDemoUndefinedVariablesOnlyClass.head
+    assert(!metricsResultDemoUndefinedVariablesOnlyClass.metricValues.exists(value => value.entityIdent.equals("Class:testclassUndev") && value.metricValue == 1.0))
+
+
+
     //TODO Test noch fehlerhaft
 
 
@@ -55,9 +70,9 @@ class NumberOfVariablesDeclaredTest extends FlatSpec with Matchers{
 
     val resultH2 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest2, appConfig, Map.empty[Symbol, Any])
     val metricResultH2 = resultH2.head
-    //assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("boolean org.h2.index.Index.mayHaveNullDuplicates(org.h2.result.SearchRow)") && value.metricValue == 5.0))
-    //assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("java.lang.String org.h2.jdbc.JdbcConnection.translateSQLImpl(java.lang.String)") && value.metricValue == 6.0))
-    //assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("void org.h2.server.TcpServer.shutdown(java.lang.String,java.lang.String,boolean,boolean)") && value.metricValue == 7.0))
+    assert(!metricResultH2.metricValues.exists(value => value.entityIdent.equals("boolean org.h2.index.Index.mayHaveNullDuplicates(org.h2.result.SearchRow)") && value.metricValue == 5.0))
+    assert(!metricResultH2.metricValues.exists(value => value.entityIdent.equals("java.lang.String org.h2.jdbc.JdbcConnection.translateSQLImpl(java.lang.String)") && value.metricValue == 6.0))
+    assert(!metricResultH2.metricValues.exists(value => value.entityIdent.equals("void org.h2.server.TcpServer.shutdown(java.lang.String,java.lang.String,boolean,boolean)") && value.metricValue == 7.0))
     assert(!metricResultH2.metricValues.exists(value => value.entityIdent.equals("Class:org/h2/engine/Setting") && value.metricValue == 14.0))
 
     val resultH2ClassOnly = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest2, appConfig, Map.empty[Symbol, Any])
