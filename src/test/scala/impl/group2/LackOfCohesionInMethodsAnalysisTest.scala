17fb45bfe4b30038c75df119fe8ea0c44c5c360d
X1999-1 <59253340+X1999-1@users.noreply.github.com>
2021-12-22 15:38:43 +0100

    LCOM-Tests erweitert

--- src/test/scala/impl/group2/LackOfCohesionInMethodsAnalysisTest.scala
+++ src/test/scala/impl/group2/LackOfCohesionInMethodsAnalysisTest.scala
@@ -6,24 +6,38 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.tud.sse.metrics.ApplicationConfiguration
 import org.tud.sse.metrics.testutils.AnalysisTestUtils
 
+/*
+ * Test fuer die LCOM-Metrik
+ * Test wird auf der "LCOM_Test1.jar"-Datei durchgefuehrt
+ */
+
 class LackOfCohesionInMethodsAnalysisTest extends FlatSpec with Matchers{
 
-  val fileToTest = new File(getClass.getResource("/group2/LCOM_Test1.jar").getPath)
-  val analysisToTest = new LackOfCohesionInMethodsAnalysis()
+  "The MethodSizeAnalysis" must "calculate valid results for single JARs" in {
 
-  val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
-    opalLoggingEnabled = false, batchModeEnabled = Some(false), excludedAnalysesNames = List(), includedAnalysesNames = List(),
-    excludeJreClasses = true, additionalClassesDir = None, loadAdditionalClassesAsInterface = false)
+    val fileToTest = new File(getClass.getResource("/group2/LCOM_Test1.jar").getPath)
+    val analysisToTest = new LackOfCohesionInMethodsAnalysis()
 
-  val result = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map.empty[Symbol, Any])
+    val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
+      opalLoggingEnabled = false, batchModeEnabled = Some(false), excludedAnalysesNames = List(), includedAnalysesNames = List(),
+      excludeJreClasses = true, additionalClassesDir = None, loadAdditionalClassesAsInterface = false)
 
-  assert(result.size == 1)
+    val result = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map.empty[Symbol, Any])
 
-  val metricResult = result.head
+    assert(result.size == 1)
 
-  assert(metricResult.success)
-  assert(metricResult.metricValues.nonEmpty)
+    val metricResult = result.head
 
-  assert(metricResult.metricValues.exists(value => value.entityIdent.equals("ErsteKlasse") && value.metricValue == 0))
-  assert(metricResult.metricValues.exists(value => value.entityIdent.equals("ZweiteKlasse") && value.metricValue == 9))
+    assert(metricResult.success)
+    assert(metricResult.metricValues.nonEmpty)
+
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("ErsteKlasse") && value.metricValue == 0))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("ZweiteKlasse") && value.metricValue == 9))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("DritteKlasse") && value.metricValue == 0))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("VierteKlasse") && value.metricValue == 1))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("FuenfteKlasse") && value.metricValue == 0))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("SechsteKlasse") && value.metricValue == 0))
+    assert(metricResult.metricValues.exists(value => value.entityIdent.equals("SiebteKlasse") && value.metricValue == 0))
+  }
+
 }
