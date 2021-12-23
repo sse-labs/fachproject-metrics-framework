aaa2a6f78400289a143e906db0501d2472ba9826
Elaf Hakim <elaf.hakim@tu-dortmund.de>
2021-12-23 13:42:16 +0100

    Optimize code

--- src/main/scala/impl/group4/LOCphyAnalysis.scala
+++ src/main/scala/impl/group4/LOCphyAnalysis.scala
@@ -10,13 +10,30 @@ import org.opalj.br.analyses.Project
 import java.net.URL
 import scala.util.Try
 
+
+/**
+ * Lines of code simply counts the lines of source code (including line break characters and comments) of a method.
+ */
+
 class LOCphyAnalysis extends MethodAnalysis{
   /**
-   * Calculates the value of the LOCphy Metric by subtracting the line numbers of the last and first elements of the line-number-table .
+   * This method is called to analyse each method individually.
+   * It calculates the value of the LOCphy Metric by subtracting the line numbers of the last and
+   * first elements of the line-number-table .
+   *
+   * @param m             is a method object in a class.
+   * @param project       Fully initialize OPAL project representing the JAR file under analysis.
+   * @param customOptions Custom analysis options taken from the CLI. Can be used to modify behavior
+   *                      of the analysis via command-line.
+   * @return Try[Iterable[MetricValue]]  object holding the intermediate result, if successful.
    */
   override def analyzeMethod(m: Method, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
+
+    val table =  m.body.get.lineNumberTable.get.lineNumbers
+
     if (project.isProjectType(m.classFile.thisType) && m.body.isDefined && m.body.get.lineNumberTable.isDefined) {
-      List(MetricValue(m.fullyQualifiedSignature, "method.locphy", m.body.get.lineNumberTable.get.lineNumbers.last.lineNumber - m.body.get.lineNumberTable.get.lineNumbers.head.lineNumber))
+      List(MetricValue(m.fullyQualifiedSignature, "method.locphy",
+       table.last.lineNumber - table.head.lineNumber))
     }
     else List.empty}
   /**
