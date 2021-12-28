82d362b83019a395cfb6c6ae1a8add1544c01473
VidRuntimes <joe.langefeld@googlemail.com>
2021-12-28 17:04:19 +0100

    h2 tests for Vref

--- src/test/scala/impl/group5/VrefAnalysisTest.scala
+++ src/test/scala/impl/group5/VrefAnalysisTest.scala
@@ -1,8 +1,9 @@
 package org.tud.sse.metrics
 package impl.group5
 
+import testutils.AnalysisTestUtils
+
 import org.scalatest.{FlatSpec, Matchers}
-import org.tud.sse.metrics.testutils.AnalysisTestUtils
 
 import java.io.File
 
@@ -15,8 +16,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
   val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
     opalLoggingEnabled = false, batchModeEnabled = Some(false), excludedAnalysesNames = List(), includedAnalysesNames = List(),
     excludeJreClasses = true, additionalClassesDir = None, loadAdditionalClassesAsInterface = false)
-  val result = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map.empty[Symbol, Any])
-  val metricResult = result.head
+  private val result = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map.empty[Symbol, Any])
+  private val metricResult = result.head
   assert(metricResult.success)
   assert(metricResult.metricValues.nonEmpty)
   assert(metricResult.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -37,8 +38,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
   assert(!metricResult.metricValues.exists(value => value.entityIdent.equals("int testclass.rechner(int,int) Anzahl der Load Field Referenzen: ") && value.metricValue == 1.0))
 
 
-  val result2 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-methoden") -> true))
-  val metricResult2 = result2.head
+  private val result2 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-methoden") -> true))
+  private val metricResult2 = result2.head
   assert(metricResult2.success)
   assert(metricResult2.metricValues.nonEmpty)
   assert(!metricResult2.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -50,8 +51,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
   assert(!metricResult2.metricValues.exists(value => value.entityIdent.equals("int testclass.rechner(int,int) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
   assert(metricResult2.metricValues.exists(value => value.entityIdent.equals("testclass Anzahl aller Null Referenzen: ") && value.metricValue == 2.0))
 
-  val result3 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-this") -> true))
-  val metricResult3 = result3.head
+  private val result3 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-this") -> true))
+  private val metricResult3 = result3.head
   assert(metricResult3.success)
   assert(metricResult3.metricValues.nonEmpty)
   assert(metricResult3.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -65,8 +66,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
 
 
 
-  val result4 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("infozuvariablen") -> true))
-  val metricResult4 = result4.head
+  private val result4 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("infozuvariablen") -> true))
+  private val metricResult4 = result4.head
   assert(metricResult4.success)
   assert(metricResult4.metricValues.nonEmpty)
   assert(metricResult4.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -85,8 +86,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
 
 
 
-  val result5 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-class") -> true))
-  val metricResult5 = result5.head
+  private val result5 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("no-class") -> true))
+  private val metricResult5 = result5.head
   assert(metricResult5.success)
   assert(metricResult5.metricValues.nonEmpty)
   assert(metricResult5.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -100,8 +101,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
 
 
 
-  val result6 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("nonullreferenz") -> true))
-  val metricResult6 = result6.head
+  private val result6 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("nonullreferenz") -> true))
+  private val metricResult6 = result6.head
   assert(metricResult6.success)
   assert(metricResult6.metricValues.nonEmpty)
   assert(!metricResult6.metricValues.exists(value => value.entityIdent.equals("void testclass.<init>(int,java.lang.String,float,java.lang.String) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
@@ -116,8 +117,8 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
 
 
 
-  val result7 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("outstoreandloadcount") -> true))
-  val metricResult7 = result7.head
+  private val result7 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map(Symbol("outstoreandloadcount") -> true))
+  private val metricResult7 = result7.head
   assert(metricResult7.success)
   assert(metricResult7.metricValues.nonEmpty)
   assert(metricResult7.metricValues.exists(value => value.entityIdent.equals("void vrefmain.main(java.lang.String[]) Anzahl der Load Field Referenzen: ") && value.metricValue == 0.0))
@@ -127,9 +128,9 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
 
 
   //PDFBOX
-  val fileToTestBox = new File(getClass.getResource("/group5/pdfbox-2.0.24.jar").getPath)
-  val resultBox = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestBox, appConfig, Map.empty[Symbol, Any])
-  val metricResultBox = resultBox.head
+  private val fileToTestBox = new File(getClass.getResource("/group5/pdfbox-2.0.24.jar").getPath)
+  private val resultBox = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestBox, appConfig, Map.empty[Symbol, Any])
+  private val metricResultBox = resultBox.head
   assert(metricResultBox.success)
   assert(metricResultBox.metricValues.nonEmpty)
   assert(metricResultBox.metricValues.exists(value => value.entityIdent.equals("org/apache/pdfbox/util/Version  Anzahl aller Referenzen der Variablen:") && value.metricValue == 16.0))
@@ -141,19 +142,30 @@ class VrefAnalysisTest extends FlatSpec with Matchers{
   assert(metricResultBox.metricValues.exists(value => value.entityIdent.equals("java.lang.String org.apache.pdfbox.util.DateConverter.toString(java.util.Calendar) Anzahl der Null Referenzen: ") && value.metricValue == 1.0))
 
   // H2 Datenbank
-  val fileToTestH2 = new File(getClass.getResource("/group5/h2-2.0.202.jar").getPath)
-  val resultH2 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestH2, appConfig, Map.empty[Symbol, Any])
-  val metricResultH2 = resultH2.head
+  private val fileToTestH2 = new File(getClass.getResource("/group5/h2-2.0.202.jar").getPath)
+  private val resultH2 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestH2, appConfig, Map.empty[Symbol, Any])
+  private val metricResultH2 = resultH2.head
+  print(metricResultH2)
   assert(metricResultH2.success)
   assert(metricResultH2.metricValues.nonEmpty)
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("org/h2/result/FetchedResult  Anzahl aller Referenzen der Variablen:") && value.metricValue == 14.0))
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("boolean org.h2.result.FetchedResult.next() Anzahl der Null Referenzen: ") && value.metricValue == 2.0))
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("org/h2/result/Sparse  Anzahl aller Referenzen der Variablen:") && value.metricValue == 38.0))
+  //this Referenzen == 0, da kein local variables table vorhanden
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("void org.h2.result.Sparse.<init>(int,int,int[]) Anzahl der this Referenzen: ") && value.metricValue == 0.0))
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("void org.h2.result.Sparse.copyFrom(org.h2.result.SearchRow) Anzahl der gesamten Variablen Referenzen : ") && value.metricValue == 13.0))
+  assert(metricResultH2.metricValues.exists(value => value.entityIdent.equals("void org.h2.result.Sparse.copyFrom(org.h2.result.SearchRow) Anzahl der Field Referenzen: ") && value.metricValue == 2.0))
+
+
+
 
 
 
 
 //GSON
-  val fileToTestGSON = new File(getClass.getResource("/group5/gson-2.8.9.jar").getPath)
-  val resultGson = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestGSON, appConfig, Map.empty[Symbol, Any])
-  val metricResultGson = resultGson.head
+  private val fileToTestGSON = new File(getClass.getResource("/group5/gson-2.8.9.jar").getPath)
+  private val resultGson = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestGSON, appConfig, Map.empty[Symbol, Any])
+  private val metricResultGson = resultGson.head
   assert(metricResultGson.success)
   assert(metricResultGson.metricValues.nonEmpty)
 }
