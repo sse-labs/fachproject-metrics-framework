48f210aebfd1cd8caa90b083f8cbb3ef4d2e5907
Helmi97 <chawachih@gmail.com>
2022-01-17 14:50:20 +0100

    code optimization/documentaion

--- src/main/scala/impl/group4/MCCCAnalysis.scala
+++ src/main/scala/impl/group4/MCCCAnalysis.scala
@@ -12,12 +12,22 @@ import java.net.URL
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Try
 
+/**
+ * MCCAnalysis implements the MCCabe Cyclomatic Complexity (MCCC) metric
+ * MCCC= E -N + 2P where E is the number of Edges, N is the number of Nodes and P is the number of connected components which is equal to 1 for single methods
+ */
 class MCCCAnalysis extends MethodAnalysis {
 
   /**
    * Calculates the MCCabe Metric by calculating the number of edges and vertices in the control flow graph
    * Since adding more vertices doesn't change e-n, each instruction is a node
    * Each node has one outgoing edge except for return instructions (0), SimpleConditionalBranchInstructions (2) and CompoundConditionalBranchInstructions(number of jumpOffsets)
+   *
+   * @param m             is a method in a class.
+   * @param project       Fully initialize OPAL project representing the JAR file under analysis.
+   * @param customOptions Custom analysis options taken from the CLI. Can be used to modify behavior
+   *                      of the analysis via command-line.
+   * @return Try[Iterable[MetricValue]]  object holding the intermediate result, if successful.
    */
   override def analyzeMethod(m: Method, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
     if (project.isProjectType(m.classFile.thisType) && m.body.isDefined) {
@@ -25,22 +35,14 @@ class MCCCAnalysis extends MethodAnalysis {
       var n = 0
       m.body.get.instructions.foreach { instruction =>
         Try {
-          if (instruction.isCompoundConditionalBranchInstruction) {
-            e = e + instruction.asCompoundConditionalBranchInstruction.jumpOffsets.size
-          }
-          else if (instruction.isSimpleConditionalBranchInstruction) {
-            e = e + 2
-          }
-          else {
-            if (!instruction.isReturnInstruction) e = e + 1
-          }
+          if (instruction.isCompoundConditionalBranchInstruction) e = e + instruction.asCompoundConditionalBranchInstruction.jumpOffsets.size
+          else if (instruction.isSimpleConditionalBranchInstruction) e = e + 2
+          else if (!instruction.isReturnInstruction) e = e + 1
           n = n + 1
         }
       }
       List(MetricValue(m.fullyQualifiedSignature, "method.mccc", e - n + 2))
-    } else {
-      List.empty
-    }
+    } else List.empty
   }
 
   /**
