10167253f7ade94a6c7de0e96b7db49144bdaea3
Philipp Knaak <philipp.knaak@tu-dortmund.de>
2022-01-17 12:21:09 +0100

    cleaning the commit

--- src/main/scala/impl/SingleFileAnalysisApplication.scala
+++ src/main/scala/impl/SingleFileAnalysisApplication.scala
@@ -4,11 +4,9 @@ package impl
 import analysis.SingleFileAnalysis
 import application.SingleFileAnalysisApplication
 
-import impl.group5.NumberOfLoopsAnalysis
 
 object SingleFileAnalysisApplication extends SingleFileAnalysisApplication {
 
   override protected val registeredAnalyses: Seq[SingleFileAnalysis] = Seq(
-    new NumberOfLoopsAnalysis()
   )
 }
