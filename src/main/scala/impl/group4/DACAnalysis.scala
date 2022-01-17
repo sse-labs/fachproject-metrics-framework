7ade3d170ac8157f11dc08663c3be7abbfb4de8a
Helmi97 <chawachih@gmail.com>
2022-01-17 14:07:25 +0100

    Optimize code

--- src/main/scala/impl/group4/DACAnalysis.scala
+++ src/main/scala/impl/group4/DACAnalysis.scala
@@ -10,11 +10,20 @@ import org.opalj.br.analyses.Project
 import java.net.URL
 import scala.util.Try
 
+/**
+ * DACAnalysis implements the DAC metric as described in https://ieeexplore.ieee.org/document/748920
+ */
+
 class DACAnalysis extends ClassFileAnalysis {
-
-
+  
   /**
-   * Calculates the value of the DAC metric by counting reference type fields in the class (see https://ieeexplore.ieee.org/document/748920)
+   * This method calculates the value of the DAC metric by counting reference type fields in the class.
+   *
+   * @param classFile     is a class in the project.
+   * @param project       Fully initialize OPAL project representing the JAR file under analysis.
+   * @param customOptions Custom analysis options taken from the CLI. Can be used to modify behavior
+   *                      of the analysis via command-line.
+   * @return Try[Iterable[MetricValue]]  object holding the intermediate result, if successful.
    */
   override def analyzeClassFile(classFile: ClassFile, project: Project[URL], customOptions: OptionMap): Try[Iterable[MetricValue]] = Try {
     var metric = 0
