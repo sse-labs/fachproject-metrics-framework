f4b0fc6c1320b3c38ab6f0077718950a2ca105cf
codesmellsfine <matthiasjapes@gmail.com>
2022-01-19 22:57:06 +0100

    Tests added and code improvements
    - Output improved
    - tests added

--- src/main/scala/impl/group1/EvolutionAnalysis.scala
+++ src/main/scala/impl/group1/EvolutionAnalysis.scala
@@ -19,16 +19,14 @@ import scala.util.control.Breaks.{break, breakable}
  *               Optional CLI arguments:
  *  - --ext_evo outputs values for partial metric external_evolution: metric determines newly added Packages in subsequent Softwareprojects
  *  - --int_evo outputs values for partial metric internal_evolution: "metric measures the ratio of packages, which interact with new packages"
- *  - --verbose log more information: file names, simple statistics, metric results
- *  - --ultra_verbose (like verbose) + list all package names and class (names) contained in each package
  *
  */
 class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,Double,String)](jarDir) {
 
   var previousFile: String = ""
   var currentFile: String = ""
-  var ext_evo: Boolean = false
-  var int_evo: Boolean = false
+  var ext_evo: Boolean = true
+  var int_evo: Boolean = true
   var previousPackages: scala.collection.Set[String] = Set[String]()
 
   var currentClassesInPackages: Map[String, Set[String]] = Map[String, Set[String]]()
@@ -61,16 +59,17 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
   override def produceAnalysisResultForJAR(project: Project[URL],
                                            lastResult: Option[(Double, Double, Double, String)],
                                            customOptions: OptionMap): Try[(Double, Double, Double, String)] = {
-    var evolution: Double = 1
-    var externalEvolution: Double = 1
-    var internalEvolution: Double = 1
+    var evolution: Double = 0
+    var externalEvolution: Double = 0
+    var internalEvolution: Double = 0
     var entityIdent: String = ""
 
     ext_evo = customOptions.contains(sym_ext_evo)
     int_evo = customOptions.contains(sym_int_evo)
 
     // external Evolution
-    // external evolution is the number of classes in newly introduced packages divided by the total amount of classes in the project.
+    // external evolution is the number of classes in newly introduced packages (@numberOfClassesInNewPackages)
+    // divided by the total amount of classes in the project (@currentNumberOfClasses).
     val currentPackages: scala.collection.Set[String] = project.projectPackages
     val currentNumberOfClasses:Double = project.projectClassFilesCount
 
@@ -90,12 +89,12 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
       log.info(s"externalEvolution: $externalEvolution")
 
       // calculate the Number of Classes for the new Packages
-
-      entityIdent = s"Difference between: $previousFile and $currentFile"
       log.info(s"entityIdent: $entityIdent")
 
       // internal Evolution
-      // internal Evolution is the number of Packages that exist in both versions and interact with newly added Packages divided by the Number of Packages that exist in both versions.
+      // internal Evolution is the number of Packages that exist in both versions and interact with newly added Packages
+      // divided by the Number of Packages that exist in both versions.
+      // interaction between packages is calculated on class level
 
 
       // calculate the denominator (maintainedPackagesSize) Number of Packages that exist in previous and current Project.
@@ -107,6 +106,7 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
       var interactionsWithNewPackages:Double = 0
 
       // iterate through classes for each Package to check if there are interactions with the classes from new Packages
+      // interaction between two classes is detected on the class level
 
       for(maintainedPackage <- maintainedPackages){
         for(newPackage <- newPackages){
@@ -117,11 +117,14 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
               for(newClass <- classesFromNewPackage){
                 maintainedClass.methods.foreach(m =>
                   if(m.body.isDefined){
+                    // interaction between two classes
                     if(m.body.get.instructions.mkString.contains(newClass.thisType.simpleName)){
                       log.info(s"maintainedPackageName: $maintainedPackage")
                       log.info(s"newPackageName: $newPackage")
                       log.info(s"Klasseninteraktion von maintainedClass: ${maintainedClass.thisType.fqn} und newClass: ${newClass.thisType.fqn}")
                       interactionsWithNewPackages = interactionsWithNewPackages +1
+                      // if there is an interaction between the maintained and new Package it will be counted once and the rest of the iterations
+                      // can be stopped, interaction between the packages is only counted once.
                       break
                     }
                   }
@@ -134,14 +137,17 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
 
       log.info(s"Maintained Packages interactions with new Packages: $interactionsWithNewPackages")
 
-      internalEvolution = interactionsWithNewPackages/maintainedPackagesSize
+      if(maintainedPackagesSize!=0){
+        internalEvolution = interactionsWithNewPackages/maintainedPackagesSize
+      }
       log.info(s"internal Evolution: $internalEvolution")
     } else{
       log.info(s"inital Round!!!")
       log.info(s"Custom Option for external Evolution is: ${ext_evo.toString}")
       log.info(s"Custom Option for internal Evolution is: ${int_evo.toString}")
-      log.info(s"inital Round!!!")
     }
+
+    entityIdent = s"Difference between: $previousFile and $currentFile "
 
     // Evolution as defined in the Paper
     evolution = (externalEvolution + internalEvolution)/2
@@ -152,7 +158,6 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
     initialRound = false
 
     Try(evolution, externalEvolution,internalEvolution,entityIdent)
-
   }
 
   /**
@@ -172,34 +177,20 @@ class EvolutionAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,Double,D
     val internalEvoMetric = analysisResultsPerFile.values.map(_.get).
       toList.map(value => MetricValue(value._4,"Internal Evolution",value._3))
 
-    val listBuffer = collection.mutable.ListBuffer[MetricsResult]()
+    val metricResultBuffer = collection.mutable.ListBuffer[MetricsResult]()
+    val metricValueBuffer = collection.mutable.ListBuffer[MetricValue]()
 
-    // Calculates the metricresult depending on the custom options ext_evo and int_evo
-    if(ext_evo && int_evo){
-      for(i <- evolutionMetric.indices){
-        listBuffer.append(MetricsResult(analysisName, jarDir, success = true,
-          metricValues = List(evolutionMetric(i), externalEvoMetric(i), internalEvoMetric(i))))
-      }
-    } else if(ext_evo) {
-      for (i <- evolutionMetric.indices) {
-        listBuffer.append(MetricsResult(analysisName, jarDir, success = true,
-          metricValues = List(evolutionMetric(i), externalEvoMetric(i))))
-      }
-    }else if(int_evo){
-        for(i <- evolutionMetric.indices){
-          listBuffer.append(MetricsResult(analysisName, jarDir, success = true,
-            metricValues = List(evolutionMetric(i), externalEvoMetric(i))))
-      }
-    } else{
-      for(i <- evolutionMetric.indices){
-        listBuffer.append(MetricsResult(analysisName, jarDir, success = true,
-          metricValues = List(evolutionMetric(i))))
-      }
-    }
+    metricValueBuffer.appendAll(evolutionMetric)
+    // with external evolution results
+    if(ext_evo)
+      metricValueBuffer.appendAll(externalEvoMetric)
+    // with internal evolution results
+    if(int_evo)
+      metricValueBuffer.appendAll(internalEvoMetric)
 
-    val metricResultList: List[MetricsResult] = listBuffer.toList
+    metricResultBuffer.append(MetricsResult(analysisName,jarDir,success = true,metricValues = metricValueBuffer.toList))
 
-    metricResultList
+    metricResultBuffer.toList
   }
 
   /**
--- src/test/scala/impl/group1/EvolutionAnalysisTest.scala
+++ src/test/scala/impl/group1/EvolutionAnalysisTest.scala
@@ -3,6 +3,9 @@ package impl.group1
 import java.io.File
 
 import org.scalatest.{FlatSpec, Matchers}
+import org.tud.sse.metrics.ApplicationConfiguration
+import org.tud.sse.metrics.analysis.MetricsResult
+import org.tud.sse.metrics.testutils.AnalysisTestUtils
 
 
 class EvolutionAnalysisTest extends FlatSpec with Matchers{
@@ -10,6 +13,125 @@ class EvolutionAnalysisTest extends FlatSpec with Matchers{
   val dirWithTestFiles = new File(getClass.getResource("/group1/commons-collections").getPath)
   val evolutionAnalysisToTest = new EvolutionAnalysis(dirWithTestFiles)
 
+  val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
+    opalLoggingEnabled = false, batchModeEnabled = Some(false), excludedAnalysesNames = List(), includedAnalysesNames = List(),
+    excludeJreClasses = true, additionalClassesDir = None, loadAdditionalClassesAsInterface = false)
 
+  val optionsMap : Map[Symbol,Any]  = Map(Symbol("ext_evo") -> "ext_evo", Symbol("int_evo")-> "int_evo")
+
+  val results:List[MetricsResult]  = AnalysisTestUtils.runMultiFileAnalysis(_ => evolutionAnalysisToTest,dirWithTestFiles,appConfig,optionsMap)
+
+  assert(results.size == 1)
+
+  val evolutionResult:MetricsResult = results.head
+  assert(evolutionResult.success)
+  assert(evolutionResult.metricValues.nonEmpty)
+
+  for(metricResult <- results){
+
+    if(diffOf0001(metricResult))
+    {
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0102(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0203(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0304(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0405(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0506(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0607(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0708(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0809(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf0910(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    } else if(diffOf1011(metricResult)){
+      assert(metricResult.metricValues.toSeq.head.metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(1).metricValue == 0.0)
+      assert(metricResult.metricValues.toSeq(2).metricValue == 0.0)
+    }
+  }
+
+  results.foreach(item => println(item.metricValues))
+
+  def diffOf0001(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("00-commons-collections-1.0.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("01-commons-collections-2.0.jar")
+  }
+
+  def diffOf0102(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("01-commons-collections-2.0.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("02-commons-collections-2.0.20020914.015953.jar")
+  }
+
+  def diffOf0203(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("02-commons-collections-2.0.20020914.015953.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("03-commons-collections-2.0.20020914.020746")
+  }
+
+  def diffOf0304(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("03-commons-collections-2.0.20020914.020746.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("04-commons-collections-2.0.20020914.020858.jar")
+  }
+
+  def diffOf0405(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("04-commons-collections-2.0.20020914.020858.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("05-commons-collections-2.1.jar")
+  }
+
+  def diffOf0506(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("05-commons-collections-2.1.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("06-commons-collections-2.1.1.jar")
+  }
+
+  def diffOf0607(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("06-commons-collections-2.1.1.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("07-commons-collections-2.0.jar")
+  }
+
+  def diffOf0708(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("07-commons-collections-3.0.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("08-commons-collections-3.1.jar")
+  }
+
+  def diffOf0809(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("08-commons-collections-3.1.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("09-commons-collections-3.2.jar")
+  }
+
+  def diffOf0910(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("09-commons-collections-3.2.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("10-commons-collections-3.2.1.jar")
+  }
+
+  def diffOf1011(result: MetricsResult): Boolean = {
+    result.metricValues.toSeq.head.entityIdent.contains("10-commons-collections-3.2.1.jar") &&
+      result.metricValues.toSeq.head.entityIdent.contains("11-commons-collections-3.2.2.jar")
+  }
 
 }
