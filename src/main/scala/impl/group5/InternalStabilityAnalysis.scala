29597ce5f26d44afacd5711ee3279c456acbaebe
sebastian <1sebasti@online.de>
2022-01-20 17:42:22 +0100

    Merge

--- src/main/scala/impl/group5/InternalStabilityAnalysis.scala
+++ src/main/scala/impl/group5/InternalStabilityAnalysis.scala
@@ -1,17 +1,20 @@
 package org.tud.sse.metrics
 package impl.group5
 
-import analysis.{MetricsResult, MultiFileAnalysis}
+import analysis.{MetricValue, MetricsResult, MultiFileAnalysis}
 
 import org.opalj.br.analyses.Project
 import org.tud.sse.metrics.input.CliParser.OptionMap
 
 import java.io.File
 import java.net.URL
+import scala.collection.mutable.ListBuffer
 import scala.util.Try
 
 class InternalStabilityAnalysis(jarDir: File) extends MultiFileAnalysis[(Double, Double, Double, String)](jarDir) {
 
+  var currentfile: String = ""
+  var previousfile: String = ""
 
   override def produceAnalysisResultForJAR(project: Project[URL], file: File,
                                            lastResult: Option[(Double, Double, Double, String)],
@@ -20,11 +23,10 @@ class InternalStabilityAnalysis(jarDir: File) extends MultiFileAnalysis[(Double,
     produceAnalysisResultForJAR(project, lastResult, customOptions)
   }
 
-  override def produceAnalysisResultForJAR(project: Project[URL],
-                                           lastResult: Option[(Double, Double, Double, String)],
-                                           customOptions: OptionMap): Try[(Double, Double, Double, String)] = {
+  override def produceAnalysisResultForJAR(project: Project[URL], lastResult: Option[(Double, Double, Double, String)], customOptions: OptionMap): Try[(Double, Double, Double, String)] = {
 
-
+    var entity_ident: String = "Difference between: " + previousfile + " and " + currentfile
+    Try(1.0, 2,3, entity_ident)
   }
 
 
