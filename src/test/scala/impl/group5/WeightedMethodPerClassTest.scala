b2da2b7c3f3c19544e8f60c5b4ff1dfc8bac4a25
VidRuntimes <joe.langefeld@googlemail.com>
2022-01-22 01:41:57 +0100

    First WMC tests

--- src/test/scala/impl/group5/WeightedMethodPerClassTest.scala
+++ src/test/scala/impl/group5/WeightedMethodPerClassTest.scala
@@ -11,7 +11,8 @@ import java.io.File
  */
 class WeightedMethodPerClassTest extends FlatSpec with Matchers{
 
-  val fileToTest = new File(getClass.getResource("/group5/pdfbox-2.0.24.jar").getPath)
+  val fileToTestPdf = new File(getClass.getResource("/group5/pdfbox-2.0.24.jar").getPath)
+  val fileToTestGrp4 = new File(getClass.getResource("/group4/maven-jar-sample-1.0-SNAPSHOT.jar").getPath)
   val analysisToTest = new WeightedMethodsPerClassAnalysis()
 
   val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
@@ -19,7 +20,30 @@ class WeightedMethodPerClassTest extends FlatSpec with Matchers{
     excludeJreClasses = true, additionalClassesDir = None, loadAdditionalClassesAsInterface = false)
 
 
-  private val resultPdf = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTest, appConfig, Map.empty[Symbol, Any])
+  private val resultPdf = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestPdf, appConfig, Map.empty[Symbol, Any])
   val metricsResultPdf = resultPdf.head
+  print(metricsResultPdf)
+  assert(metricsResultPdf.success)
+  assert(metricsResultPdf.metricValues.nonEmpty)
+  assert(metricsResultPdf.metricValues.exists(value => value.entityIdent.equals("WMC von org/apache/pdfbox/text/PDFTextStripperByArea") && value.metricValue == 14.0))
+  assert(metricsResultPdf.metricValues.exists(value => value.entityIdent.equals("WMC von org/apache/pdfbox/pdmodel/graphics/shading/Type7ShadingContext") && value.metricValue == 1.0))
+  assert(metricsResultPdf.metricValues.exists(value => value.entityIdent.equals("WMC von org/apache/pdfbox/pdmodel/encryption/MessageDigests") && value.metricValue == 4.0))
+
+
+  private val resultGrp4 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestGrp4, appConfig, Map.empty[Symbol, Any])
+  val metricsResultGrp4 = resultGrp4.head
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/MCCCAnalysisTest") && value.metricValue == 12.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/RFCAnalysisTest1") && value.metricValue == 4.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/RFCAnalysisTest3") && value.metricValue == 1.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/RFCAnalysisTest2") && value.metricValue == 7.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/DACAnalysisTest2") && value.metricValue == 1.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/DACAnalysisTest1") && value.metricValue == 1.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/DACAnalysisTest3") && value.metricValue == 2.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/LOCphyAnalysisTest") && value.metricValue == 7.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von dem kompletten Projekt: ") && value.metricValue == 35.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC Durschnitt von allen Projekt Klassen: ") && value.metricValue == 4.0))
+  assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("Höchster WMC Wert im Projekt ist in Klasse: com/group4/sample/MCCCAnalysisTest") && value.metricValue == 12.0))
+
+
 
 }
