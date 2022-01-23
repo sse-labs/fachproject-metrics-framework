474483230deee2c317ee00b6f372f6d09e0790d8
VidRuntimes <joe.langefeld@googlemail.com>
2022-01-23 12:45:30 +0100

    WMC Gson tests

--- src/test/scala/impl/group5/WeightedMethodPerClassTest.scala
+++ src/test/scala/impl/group5/WeightedMethodPerClassTest.scala
@@ -13,6 +13,7 @@ class WeightedMethodPerClassTest extends FlatSpec with Matchers{
 
   val fileToTestPdf = new File(getClass.getResource("/group5/pdfbox-2.0.24.jar").getPath)
   val fileToTestGrp4 = new File(getClass.getResource("/group4/maven-jar-sample-1.0-SNAPSHOT.jar").getPath)
+  val fileToTestGson = new File(getClass.getResource("/group5/gson-2.8.9.jar").getPath)
   val analysisToTest = new WeightedMethodsPerClassAnalysis()
 
   val appConfig = new ApplicationConfiguration(inputFilePath = "", treatFilesAsLibrary = true, outFileOption = None,
@@ -21,8 +22,7 @@ class WeightedMethodPerClassTest extends FlatSpec with Matchers{
 
 
   private val resultPdf = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestPdf, appConfig, Map.empty[Symbol, Any])
-  val metricsResultPdf = resultPdf.head
-  print(metricsResultPdf)
+  private val metricsResultPdf = resultPdf.head
   assert(metricsResultPdf.success)
   assert(metricsResultPdf.metricValues.nonEmpty)
   assert(metricsResultPdf.metricValues.exists(value => value.entityIdent.equals("WMC von org/apache/pdfbox/text/PDFTextStripperByArea") && value.metricValue == 14.0))
@@ -30,8 +30,17 @@ class WeightedMethodPerClassTest extends FlatSpec with Matchers{
   assert(metricsResultPdf.metricValues.exists(value => value.entityIdent.equals("WMC von org/apache/pdfbox/pdmodel/encryption/MessageDigests") && value.metricValue == 4.0))
 
 
+  private val resultGson = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestGson, appConfig, Map.empty[Symbol, Any])
+  private val metricsResultGson = resultGson.head
+  assert(metricsResultGson.success)
+  assert(metricsResultGson.metricValues.nonEmpty)
+  assert(metricsResultGson.metricValues.exists(value => value.entityIdent.equals("WMC von com/google/gson/internal/JavaVersion") && value.metricValue == 11.0))
+  assert(metricsResultGson.metricValues.exists(value => value.entityIdent.equals("WMC von com/google/gson/internal/bind/ObjectTypeAdapter") && value.metricValue == 9.0))
+  assert(metricsResultGson.metricValues.exists(value => value.entityIdent.equals("WMC von com/google/gson/FieldNamingStrategy") && value.metricValue == 0.0))
+
+
   private val resultGrp4 = AnalysisTestUtils.runSingleFileAnalysis(analysisToTest, fileToTestGrp4, appConfig, Map.empty[Symbol, Any])
-  val metricsResultGrp4 = resultGrp4.head
+  private val metricsResultGrp4 = resultGrp4.head
   assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/MCCCAnalysisTest") && value.metricValue == 12.0))
   assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/RFCAnalysisTest1") && value.metricValue == 4.0))
   assert(metricsResultGrp4.metricValues.exists(value => value.entityIdent.equals("WMC von com/group4/sample/RFCAnalysisTest3") && value.metricValue == 1.0))
