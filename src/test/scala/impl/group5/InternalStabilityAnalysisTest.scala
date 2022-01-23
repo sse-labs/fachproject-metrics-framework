39c5a69adfadcce411a0f92ea45842247f3f89ba
sebastian <1sebastian1@online.de>
2022-01-23 22:44:59 +0100

    Stelle Tests fertig

--- src/test/scala/impl/group5/InternalStabilityAnalysisTest.scala
+++ src/test/scala/impl/group5/InternalStabilityAnalysisTest.scala
@@ -15,22 +15,45 @@ class InternalStabilityAnalysisTest extends FlatSpec with Matchers{
   val filesToTestjars = new File(getClass.getResource("/group5/testJars").getPath)
   val analysisToTestjars = new InternalStabilityAnalysis(filesToTestjars)
   val resultjars  = AnalysisTestUtils.runMultiFileAnalysis(_ => analysisToTestjars,filesToTestjars,appConfig,Map.empty[Symbol, Any])
+  val metricResultjars = resultjars.head
+  assert(metricResultjars.success)
+  assert(metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.0.jar and testInternalST-2.1.jar") && value.metricValue == 1.0))
+  assert(metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") && value.metricValue == 0.96875))
+  assert(metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.0.jar and testInternalST-3.1.jar") && value.metricValue == 0.9375))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-1.2.jar and testInternalST-2.0.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 4))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-1.2.jar and testInternalST-2.0.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 4))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 7.5))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 7))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.1.jar and testInternalST-3.2.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 6.5))
+  assert(!metricResultjars.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.1.jar and testInternalST-3.2.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 7))
+  val filesToTestjars2 = new File(getClass.getResource("/group5/testJars").getPath)
+  val analysisToTestjars2 = new InternalStabilityAnalysis(filesToTestjars)
+  val resultjars2  = AnalysisTestUtils.runMultiFileAnalysis(_ => analysisToTestjars,filesToTestjars,appConfig,Map(Symbol("all_value") -> true))
+  val metricResultjars2 = resultjars2.head
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.0.jar and testInternalST-2.1.jar") && value.metricValue == 1.0))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") && value.metricValue == 0.96875))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.0.jar and testInternalST-3.1.jar") && value.metricValue == 0.9375))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-1.2.jar and testInternalST-2.0.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 4))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-1.2.jar and testInternalST-2.0.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 4))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 7.5))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-2.1.jar and testInternalST-2.2.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 8.0))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.1.jar and testInternalST-3.2.jar") &&value.metricName.equals("PREL_Remove") && value.metricValue == 6.5))
+  assert(metricResultjars2.metricValues.exists(value => value.entityIdent.equals("Difference between: testInternalST-3.1.jar and testInternalST-3.2.jar") &&value.metricName.equals("PREL_Append") && value.metricValue == 7.0))
 
 
 
 
-  val filesToTest = new File(getClass.getResource("/group1/commons-collections").getPath)
-  val analysisToTest = new InternalStabilityAnalysis(filesToTest)
-  val result  = AnalysisTestUtils.runMultiFileAnalysis(_ => analysisToTest,filesToTest,appConfig,Map.empty[Symbol, Any])
+
 
 
 
   val filesToTestGson = new File(getClass.getResource("/group5/gson").getPath)
   val analysisToTestGson = new InternalStabilityAnalysis(filesToTestGson)
   val resultGson  = AnalysisTestUtils.runMultiFileAnalysis(_ => analysisToTestGson,filesToTestGson,appConfig,Map.empty[Symbol, Any])
-
-
-
+  val metricResultGson = resultGson.head
+  assert(metricResultGson.success)
+  assert(metricResultGson.metricValues.exists(value => value.entityIdent.equals("Difference between: gson-1.4.jar and gson-1.5.jar") && value.metricValue == 0.9449349481258368))
+  assert(metricResultGson.metricValues.exists(value => value.entityIdent.equals("Difference between: gson-2.2.jar and gson-2.4.jar") && value.metricValue == 0.91516736266809))
 
   println("Bye")
 
