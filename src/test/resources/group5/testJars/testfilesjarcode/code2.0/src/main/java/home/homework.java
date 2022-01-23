c770f23f88342374e9ccf937eac59eff934e150f
sebastian <1sebastian1@online.de>
2022-01-23 21:50:46 +0100

    Testfiles und source code

---
+++ src/test/resources/group5/testJars/testfilesjarcode/code2.0/src/main/java/home/homework.java
@@ -0,0 +1,12 @@
+package home;
+
+import fast.Bye;
+
+public class homework {
+
+    public void home()
+    {
+        Bye bye = new Bye();
+        System.out.println(bye.byeS());
+    }
+}
---
+++ src/test/resources/group5/testJars/testfilesjarcode/code3.2/src/main/java/home/homework.java
@@ -0,0 +1,15 @@
+package home;
+
+import fast.Bye;
+import work.work1;
+import work.work2;
+
+public class homework {
+
+    public void home()
+    {
+        Bye bye = new Bye();
+        work2 work3 = new work2();
+        work3.endework("HH");
+    }
+}
