6aa7e938cb8efa6275759d5843f673e1130aa76a
David Nieder <post@davidnieder.de>
2022-01-25 16:41:28 +0100

    clean up application files

--- src/main/scala/impl/MultiFileAnalysisApplication.scala
+++ src/main/scala/impl/MultiFileAnalysisApplication.scala
@@ -3,18 +3,17 @@ package impl
 
 import analysis.MultiFileAnalysis
 import application.MultiFileAnalysisApplication
-import impl.group1
-import impl.group1.{EvolutionAnalysis, ExternalStabilityAnalysis}
 
-import org.tud.sse.metrics.impl.group5.InternalStabilityAnalysis
+import impl.group1.{EvolutionAnalysis, ExternalStabilityAnalysis}
+import impl.group5.InternalStabilityAnalysis
 
 import java.io.File
 
 object MultiFileAnalysisApplication extends MultiFileAnalysisApplication {
   override protected def buildAnalyses(jarDirectory: File): Seq[MultiFileAnalysis[_]] = Seq(
-    new InternalStabilityAnalysis(jarDirectory),
     new EvolutionAnalysis(jarDirectory),
-    new ExternalStabilityAnalysis(jarDirectory)
+    new ExternalStabilityAnalysis(jarDirectory),
 
+    new InternalStabilityAnalysis(jarDirectory)
   )
 }
